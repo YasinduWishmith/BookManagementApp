8b590a45b1fd7dd62f5eac5c114f10c7d7ff5d8a
yasindu <yasinduwishmith97@gmail.com>
2021-09-16 13:44:24 +0530

    edit book

--- book-management-app/src/components/BookList.js
+++ book-management-app/src/components/BookList.js
@@ -3,7 +3,26 @@ import _ from 'lodash';
 import Book from "./Book";
 
 const BookList = ({books, setBooks}) => {
-    return <h2> List Of Books</h2>
+   
+    const handleRemoveBook = (id) => {
+        setBooks(books.filter((book) => book.id !== id));
+    };
+
+    return (
+        <React.Fragment>
+            <div className = "book-list">
+                {
+                    !_.isEmpty(books) ? (
+                        books.map((book) => (
+                            <Book key = {book.id} {...book} handleRemoveBook = {handleRemoveBook}/>
+                        ))
+                    ) : (
+                        <p className = "message"> No books available. Please add some books</p>
+                    )
+                }
+            </div>
+        </React.Fragment>
+    );
 }
 
 export default BookList;
--- book-management-app/src/router/AppRouter.js
+++ book-management-app/src/router/AppRouter.js
@@ -1,34 +1,43 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { Redirect } from "react-router";
 import Header from "../components/Header";
 import AddBook from "../components/AddBook";
 import BookList from "../components/BookList";
 import useLocalStorage from "../Hooks/useLocalStorage";
+import EditBook from "../components/EditBook";
 
 const AppRouter = () => {
 
-        const [books, setBooks] = useLocalStorage('books', []);
+    const [books, setBooks] = useLocalStorage('books', []);
 
     return (
         <BrowserRouter>
-        <div>
-            <Header />
-            <Switch>
-                <Route 
-                render = {(props) => (
-                    <BookList {...props} books = { books } setBooks = {setBooks} />
-                )}
-                path = "/" 
-                exact = {true} 
-                />
-                <Route
-                 render = {(props) => (
-                     <AddBook {...props} books = {books} setBooks = {setBooks} />
-                 )}
-                 path = "/add"
-                />
-            </Switch>
-        </div>
+            <div>
+                <Header />
+                <Switch>
+                    <Route
+                        render={(props) => (
+                            <BookList {...props} books={books} setBooks={setBooks} />
+                        )}
+                        path="/"
+                        exact={true}
+                    />
+                    <Route
+                        render={(props) => (
+                            <AddBook {...props} books={books} setBooks={setBooks} />
+                        )}
+                        path="/add"
+                    />
+                    <Route
+                        render={(props) => (
+                            <EditBook {...props} books={books} setBooks={setBooks} />
+                        )}
+                        path="/edit/:id"
+                    />
+                    <Route component = {() => <Redirect to = "/"/>}/>
+                </Switch>
+            </div>
         </BrowserRouter>
     );
 };
